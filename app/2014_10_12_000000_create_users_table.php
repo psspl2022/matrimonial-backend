853026bfa397763d9dcb0c96728f32ec8141ebba
DishaBhandari <dishabhandari0309@gmail.com>
2022-09-10 10:59:37 +0530

    Create 2014_10_12_000000_create_users_table.php
    commit

---
+++ app/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('user_type', ['0','1', '2', '3'])->default('0')->comment('0=NULL, 1=User, 2=Admin, 3=Relationsip Manager');
+            $table->integer('admin_reg_id')->nullable();
+            $table->integer('user_reg_id')->nullable();
+            $table->string('name')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('otp')->nullable();
+            $table->string('password');
+            $table->enum('status', ['0', '1'])->default('1')->comment('0=Inactive, 1=Active');
+            $table->rememberToken();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
