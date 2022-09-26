039b7ef03b70686ed06598c685dc13f1e1a1080b
surajofficial <surajofficial694@gmail.com>
2022-09-26 13:28:18 +0530

    desire done

--- app/Http/Controllers/DesireTest.php
+++ app/Http/Controllers/DesireTest.php
@@ -20,31 +20,47 @@ class DesireTest extends Controller
 {
     public function showDesiredProfiles(Request $req)
     {
-        // return $req->all();
-        // $reg_id = 4;
-        // $data1 = DesiredProfile::where('reg_id', $reg_id)->first();
-        // $data3 = DB::table('basic_details')->select(['height', 'caste', DB::raw("(case when (height > 21)  then 1 else 0 end) as present"), DB::raw("(case when (caste = 1) then 1  else 0 end) as cast"), DB::raw("SUM(present,cast) as total")])->get()->take(4);
+
         $reg_id = 4;
         $data1 = DesiredProfile::where('reg_id', $reg_id)->first();
         $gender = (int)(UserRegister::where('id', $reg_id)->first('gender'))->gender;
         // mother tounge
-        $moth = explode(',', $req->moth);
+        // $moth = explode(',', $req->moth);
+        $moth = $req->moth;
         // Religion 
-        $relgion = explode(',', $req->religion);
+        // $relgion = explode(',', $req->religion);
+        $relgion = $req->religion;
         // marital status 
-        $martital = explode(',', $req->martital);
+        // $martital = explode(',', $req->martital);
+        $martital = $req->martital;
         // if mother tongue value nul set thius value
-        if ($req->moth == 'null') {
+        if ($req->moth == 'null' || empty($req->martital)) {
             $moth = BasicDetail::get('mother_tongue');
+            $newmoth = "";
+            foreach ($moth as $item) {
+                $newmoth .= $item->mother_tongue . ",";
+            }
+            $moth = rtrim($newmoth, ", ");
         }
+        // $req->minincome = 1;
+        // $req->maxincome = 2;
         // if religion value nul set thius value
-        if ($req->religion == 'null') {
+        if ($req->religion == 'null' || empty($req->martital)) {
             $relgion = BasicDetail::get('religion');
+            $newrelgion = "";
+            foreach ($relgion as $item) {
+                $newrelgion .= $item->religion . ",";
+            }
+            $relgion = rtrim($newrelgion, ", ");
         }
         // if marital statu value nul set thius value
-        if ($req->martital == 'null') {
-            $martital = ['0', '1', '2', '3', '4', '5'];
+        if ($req->martital == 'null' || empty($req->martital)) {
+            // $martital = ['0', '1', '2', '3', '4', '5'];
+            $martital = "0, 1, 2, 3, 4, 3";
         }
+        // return $martital;
+        // return response()->json($result, 200);
+        // exit();
         $t = 15;
         if ($data1->min_height == null || $data1->max_height == null) {
             $data1->min_height = 0;
@@ -110,33 +126,48 @@ class DesireTest extends Controller
         }
         $offset = ($req->page == 0 ? $req->page : ($req->page - 1) * 4);
         $offset = $req->page * 4;
-        if ($gender == 1  || $gender == 2) {
-            $result = DB::select("Select b.reg_id, (100*(IF(b.height between $data1->min_height and $data1->max_height ,1, 0)+IF(b.caste IN( $data1->caste), 1, 0)+IF(b.marital_status IN( $data1->marital), 1, 0)+IF(b.religion IN( $data1->religion), 1, 0)+IF(b.mother_tongue IN( $data1->mother_tongue), 1, 0)+IF(b.manglik IN( $data1->manglik), 1, 0)+IF(b.country IN( $data1->country), 1, 0)+IF(b.residence IN( $data1->residential), 1, 0)+IF(c.income between $data1->min_income and $data1->max_income, 1, 0)+IF(c.highest_qualification IN( $data1->highest_education), 1, 0)+IF(c.occupation IN( $data1->occupation), 1, 0)+IF(l.diet_habit IN( $data1->diet), 1, 0)+IF(l.drink_habit IN( $data1->drinking), 1, 0)+IF(l.smoking_habit IN( $data1->smoking), 1, 0)+IF(l.challenged IN( $data1->challenged), 1, 0))/$t) as percentage from basic_details b left join career_details c on b.reg_id = c.reg_id left join lifestyle_details l on b.reg_id = l.reg_id  left join user_registers u on b.reg_id = u.id where b.reg_id != $reg_id and u.gender != $gender HAVING percentage > 0  order by percentage desc,b.reg_id desc limit $offset,4");
-            $ids = DB::select("Select (100*(IF(b.height between $data1->min_height and $data1->max_height ,1, 0)+IF(b.caste IN( $data1->caste), 1, 0)+IF(b.marital_status IN( $data1->marital), 1, 0)+IF(b.religion IN( $data1->religion), 1, 0)+IF(b.mother_tongue IN( $data1->mother_tongue), 1, 0)+IF(b.manglik IN( $data1->manglik), 1, 0)+IF(b.country IN( $data1->country), 1, 0)+IF(b.residence IN( $data1->residential), 1, 0)+IF(c.income between $data1->min_income and $data1->max_income, 1, 0)+IF(c.highest_qualification IN( $data1->highest_education), 1, 0)+IF(c.occupation IN( $data1->occupation), 1, 0)+IF(l.diet_habit IN( $data1->diet), 1, 0)+IF(l.drink_habit IN( $data1->drinking), 1, 0)+IF(l.smoking_habit IN( $data1->smoking), 1, 0)+IF(l.challenged IN( $data1->challenged), 1, 0))/$t) as percentage from basic_details b left join career_details c on b.reg_id = c.reg_id left join lifestyle_details l on b.reg_id = l.reg_id  left join user_registers u on b.reg_id = u.id where b.reg_id != $reg_id and u.gender != $gender HAVING percentage > 0  order by percentage desc,b.reg_id desc");
-        } else {
-            $result = DB::select("Select b.reg_id, (100*(IF(b.height between $data1->min_height and $data1->max_height ,1, 0)+IF(b.caste IN( $data1->caste), 1, 0)+IF(b.marital_status IN( $data1->marital), 1, 0)+IF(b.religion IN( $data1->religion), 1, 0)+IF(b.mother_tongue IN( $data1->mother_tongue), 1, 0)+IF(b.manglik IN( $data1->manglik), 1, 0)+IF(b.country IN( $data1->country), 1, 0)+IF(b.residence IN( $data1->residential), 1, 0)+IF(c.income between $data1->min_income and $data1->max_income, 1, 0)+IF(c.highest_qualification IN( $data1->highest_education), 1, 0)+IF(c.occupation IN( $data1->occupation), 1, 0)+IF(l.diet_habit IN( $data1->diet), 1, 0)+IF(l.drink_habit IN( $data1->drinking), 1, 0)+IF(l.smoking_habit IN( $data1->smoking), 1, 0)+IF(l.challenged IN( $data1->challenged), 1, 0))/$t) as percentage from basic_details b left join career_details c on b.reg_id = c.reg_id left join lifestyle_details l on b.reg_id = l.reg_id  left join user_registers u on b.reg_id = u.id where b.reg_id != $reg_id HAVING percentage > 0  order by percentage desc,b.reg_id desc limit $offset,4");
-        }
-        // return $data2;
-        // $count = count($data2);
-        // $result = DB::select("Select reg_id, height, caste, (100*(IF(height > 21,1, 0)+IF(caste = 1, 1, 0))/4) as percentage from basic_details  order by percentage desc,reg_id desc limit 2 , 4");
-
-        $x = [];
-        $k = 0;
-        foreach ($result as $i) {
-            $x[$k] = $i->reg_id;
-            $k++;
-        }
-        $allData = BasicDetail::with('getInterestSent:reg_id,reg_id,revert', 'getShortlist:id,saved_reg_id', 'getProfileImage:by_reg_id,identity_card_doc', 'getIncome:incomes.income', 'getOccupation:occupations.occupation', 'getEducation:educations.education', 'getHeight:id,height', 'getReligion:id,religion', 'getCaste:id,caste', 'getMotherTongue:id,mother_tongue', 'getCity:id,name')->select('reg_id', 'name', 'dob', 'height', 'religion', 'caste', 'mother_tongue', 'city')->whereIn('reg_id', $x)->get();
-        $newdata = [];
-        $index = 0;
-        foreach ($result as $i) {
-            foreach ($allData as $item) {
-                if ($item->reg_id == $i->reg_id) {
-                    $item->percentage = ceil($i->percentage);
-                    $newdata[$index] = $item;
-                    $index++;
-                }
+        $offset = $req->page * 4;
+        $minage = $req->minage . ' 00:00:00';
+        $maxage = $req->maxage . ' 00:00:00';
+        if (!empty($data1)) {
+            if ($gender == 1  || $gender == 2) {
+                $result = DB::select("Select b.reg_id, (100*(IF(b.height between $data1->min_height and $data1->max_height ,1, 0)+IF(b.caste IN( $data1->caste), 1, 0)+IF(b.marital_status IN( $data1->marital), 1, 0)+IF(b.religion IN( $data1->religion), 1, 0)+IF(b.mother_tongue IN( $data1->mother_tongue), 1, 0)+IF(b.manglik IN( $data1->manglik), 1, 0)+IF(b.country IN( $data1->country), 1, 0)+IF(b.residence IN( $data1->residential), 1, 0)+IF(c.income between $data1->min_income and $data1->max_income, 1, 0)+IF(c.highest_qualification IN( $data1->highest_education), 1, 0)+IF(c.occupation IN( $data1->occupation), 1, 0)+IF(l.diet_habit IN( $data1->diet), 1, 0)+IF(l.drink_habit IN( $data1->drinking), 1, 0)+IF(l.smoking_habit IN( $data1->smoking), 1, 0)+IF(l.challenged IN( $data1->challenged), 1, 0))/$t) as percentage from basic_details b left join career_details c on b.reg_id = c.reg_id left join lifestyle_details l on b.reg_id = l.reg_id  left join user_registers u on b.reg_id = u.id where b.reg_id != $reg_id and u.gender != $gender and b.mother_tongue IN($moth) and b.religion IN($relgion) and b.marital_status IN($martital) and c.income >= $req->minincome and c.income <= $req->maxincome and b.height >= $req->minheight and b.height <= $req->maxheight and b.dob <= '$minage' and b.dob >= '$maxage'   HAVING percentage > 0  order by percentage desc,b.reg_id desc limit $offset,4");
+                $ids = DB::select("Select b.reg_id, (100*(IF(b.height between $data1->min_height and $data1->max_height ,1, 0)+IF(b.caste IN( $data1->caste), 1, 0)+IF(b.marital_status IN( $data1->marital), 1, 0)+IF(b.religion IN( $data1->religion), 1, 0)+IF(b.mother_tongue IN( $data1->mother_tongue), 1, 0)+IF(b.manglik IN( $data1->manglik), 1, 0)+IF(b.country IN( $data1->country), 1, 0)+IF(b.residence IN( $data1->residential), 1, 0)+IF(c.income between $data1->min_income and $data1->max_income, 1, 0)+IF(c.highest_qualification IN( $data1->highest_education), 1, 0)+IF(c.occupation IN( $data1->occupation), 1, 0)+IF(l.diet_habit IN( $data1->diet), 1, 0)+IF(l.drink_habit IN( $data1->drinking), 1, 0)+IF(l.smoking_habit IN( $data1->smoking), 1, 0)+IF(l.challenged IN( $data1->challenged), 1, 0))/$t) as percentage from basic_details b left join career_details c on b.reg_id = c.reg_id left join lifestyle_details l on b.reg_id = l.reg_id  left join user_registers u on b.reg_id = u.id where b.reg_id != $reg_id and u.gender != $gender and b.mother_tongue IN($moth) and b.religion IN($relgion) and b.marital_status IN($martital) and c.income >= $req->minincome and c.income <= $req->maxincome and b.height >= $req->minheight and b.height <= $req->maxheight and b.dob <= '$minage' and b.dob >= '$maxage'   HAVING percentage > 0  order by percentage desc,b.reg_id desc");
+                // return response()->json(, 200);
+                // exit();
+            } else {
+                $result = DB::select("Select b.reg_id, (100*(IF(b.height between $data1->min_height and $data1->max_height ,1, 0)+IF(b.caste IN( $data1->caste), 1, 0)+IF(b.marital_status IN( $data1->marital), 1, 0)+IF(b.religion IN( $data1->religion), 1, 0)+IF(b.mother_tongue IN( $data1->mother_tongue), 1, 0)+IF(b.manglik IN( $data1->manglik), 1, 0)+IF(b.country IN( $data1->country), 1, 0)+IF(b.residence IN( $data1->residential), 1, 0)+IF(c.income between $data1->min_income and $data1->max_income, 1, 0)+IF(c.highest_qualification IN( $data1->highest_education), 1, 0)+IF(c.occupation IN( $data1->occupation), 1, 0)+IF(l.diet_habit IN( $data1->diet), 1, 0)+IF(l.drink_habit IN( $data1->drinking), 1, 0)+IF(l.smoking_habit IN( $data1->smoking), 1, 0)+IF(l.challenged IN( $data1->challenged), 1, 0))/$t) as percentage from basic_details b left join career_details c on b.reg_id = c.reg_id left join lifestyle_details l on b.reg_id = l.reg_id  left join user_registers u on b.reg_id = u.id where b.reg_id != $reg_id and b.mother_tongue IN($moth) and b.religion IN($relgion) and b.marital_status IN($martital) and c.income >= $req->minincome and c.income <= $req->maxincome and b.height >= $req->minheight and b.height <= $req->maxheight and b.dob <= '$minage' and b.dob >= '$maxage'   HAVING percentage > 0  order by percentage desc,b.reg_id desc limit $offset,4");
+                $ids = DB::select("Select b.reg_id, (100*(IF(b.height between $data1->min_height and $data1->max_height ,1, 0)+IF(b.caste IN( $data1->caste), 1, 0)+IF(b.marital_status IN( $data1->marital), 1, 0)+IF(b.religion IN( $data1->religion), 1, 0)+IF(b.mother_tongue IN( $data1->mother_tongue), 1, 0)+IF(b.manglik IN( $data1->manglik), 1, 0)+IF(b.country IN( $data1->country), 1, 0)+IF(b.residence IN( $data1->residential), 1, 0)+IF(c.income between $data1->min_income and $data1->max_income, 1, 0)+IF(c.highest_qualification IN( $data1->highest_education), 1, 0)+IF(c.occupation IN( $data1->occupation), 1, 0)+IF(l.diet_habit IN( $data1->diet), 1, 0)+IF(l.drink_habit IN( $data1->drinking), 1, 0)+IF(l.smoking_habit IN( $data1->smoking), 1, 0)+IF(l.challenged IN( $data1->challenged), 1, 0))/$t) as percentage from basic_details b left join career_details c on b.reg_id = c.reg_id left join lifestyle_details l on b.reg_id = l.reg_id  left join user_registers u on b.reg_id = u.id where b.reg_id != $reg_id and b.mother_tongue IN($moth) and b.religion IN($relgion) and b.marital_status IN($martital) and c.income >= $req->minincome and c.income <= $req->maxincome and b.height >= $req->minheight and b.height <= $req->maxheight and b.dob <= '$minage' and b.dob >= '$maxage'   HAVING percentage > 0  order by percentage desc,b.reg_id desc");
             }
+
+
+            if (!empty($result)) {
+                $x = [];
+                $k = 0;
+                foreach ($result as $i) {
+                    $x[$k] = $i->reg_id;
+                    $k++;
+                }
+                $allData = BasicDetail::with('getInterestSent:reg_id,reg_id,revert', 'getShortlist:id,saved_reg_id', 'getProfileImage:by_reg_id,identity_card_doc', 'getIncome:incomes.income', 'getOccupation:occupations.occupation', 'getEducation:educations.education', 'getHeight:id,height', 'getReligion:id,religion', 'getCaste:id,caste', 'getMotherTongue:id,mother_tongue', 'getCity:id,name')->select('reg_id', 'name', 'dob', 'height', 'religion', 'caste', 'mother_tongue', 'city')->whereIn('reg_id', $x)->get();
+                $newdata = [];
+                $index = 0;
+                foreach ($result as $i) {
+                    foreach ($allData as $item) {
+                        if ($item->reg_id == $i->reg_id) {
+                            $item->percentage = ceil($i->percentage);
+                            $newdata[$index] = $item;
+                            $index++;
+                        }
+                    }
+                }
+            } else {
+                $data = ['msg' => 'No Data Found!!'];
+                $data['income'] = $req->minage > "2001-05-31 00:00:00";
+                $data['income2'] = $req->minage . " >" . "2001-05-31 00:00:00";
+                return response()->json($data, 201);
+            }
+        } else {
+            return response()->json(['msg' => 'Please, Fill desired details first!!'], 201);
         }
         // counting number of time loop runing 
         $ids = count($ids);
@@ -147,6 +178,7 @@ class DesireTest extends Controller
         $current = 0;
         // key for total pagination page 
         $key = [0];
+        $hk = "";
         for ($i = 1; $i <= $ids; $i++) {
             // whene loop reach division of 4 
             $hk[$i - 1] = $i;
@@ -169,6 +201,8 @@ class DesireTest extends Controller
         $data['page'] = $current;
         $data['id'] = $ids;
         $data['hk'] = $hk;
+        $data['income'] = $req->minage > "2001-05-31 00:00:00";
+        $data['income2'] = $req->minage . " >" . "2001-05-31 00:00:00";
         return response()->json($data, 200);
     }
 }
